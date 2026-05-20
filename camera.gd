82a8a23a3d7f2082da67467376cdb77bfa21e812
Avishi Sharma <robloxpusheen8@gmail.com>
2026-05-20 08:58:54 -0400

    camera can now center!!!

--- camera.gd
+++ camera.gd
@@ -5,6 +5,9 @@ var is_anim_running:bool = false
 var is_going_right:bool = false
 var is_going_left:bool = false
 
+var is_on_right:bool = false
+var is_on_left:bool = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
@@ -17,17 +20,41 @@ func _process(delta: float) -> void:
 	if cursor_x >= 0 and cursor_x <= screen_x:
 		if cursor_x >= screen_x-100 and cursor_x <= screen_x and !is_going_right:
 			var tween = create_tween()
-			tween.tween_property(self, "rotation_degrees:y", -75.3,2)
 			is_going_right = true
-			tween.tween_callback(func():
-				tween.kill()
-				is_going_right=false
-			)
+			
+			if is_on_left: #center if so
+				is_on_left=false
+				tween.tween_property(self, "rotation_degrees:y", 0, 1.5)
+				tween.tween_callback(func():
+					tween.kill()
+					is_going_right=false
+					is_on_right=false
+				)
+				
+			else: #alll the way to the right
+				tween.tween_property(self, "rotation_degrees:y", -75.3,2)
+				tween.tween_callback(func():
+					tween.kill()
+					is_going_right=false
+					is_on_right=true
+				)
 		elif cursor_x <= 100 and cursor_x >= 0 and !is_going_left:
 			var tween = create_tween()
-			tween.tween_property(self, "rotation_degrees:y", 75.3,2)
 			is_going_left = true
-			tween.tween_callback(func():
-				tween.kill()
-				is_going_left=false
-			)
+			
+			if is_on_right: #center...
+				is_on_right=false
+				tween.tween_property(self, "rotation_degrees:y", 0, 1.5)
+				tween.tween_callback(func():
+					tween.kill()
+					is_going_left=false
+					is_on_left=false
+				)
+				
+			else: #slideee to the left
+				tween.tween_property(self, "rotation_degrees:y", 75.3,2)
+				tween.tween_callback(func():
+					tween.kill()
+					is_going_left=false
+					is_on_left=true
+				)
