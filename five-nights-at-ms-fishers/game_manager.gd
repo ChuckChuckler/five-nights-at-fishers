0d8e979fa2f79bae4bca990cdebb9ab54200197e
Avishi Sharma <robloxpusheen8@gmail.com>
2026-05-27 17:05:09 -0400

    starting a security camera system i suppose

--- five-nights-at-ms-fishers/game_manager.gd
+++ five-nights-at-ms-fishers/game_manager.gd
@@ -5,9 +5,12 @@ var current_hour:int=0
 const START_POWER:float=1000.0
 var current_power:float=1000.0
 
+@onready var room = $"../room"
+@onready var cam = room.get_node("SubViewport")
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	$"../cameras/TextureRect".texture=cam.get_texture()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -23,3 +26,11 @@ func _next_hour() -> void:
 func _timeout_counter() -> void:
 	current_power-=$"../gui/usage_bar_container".power_sources
 	$"../gui/power_left".text="Power left: " + str(int(current_power/START_POWER*100)) + " %"
+
+
+func _open_camera() -> void:
+	$"../cameras".visible=true
+
+
+func _close_camera() -> void:
+	$"../cameras".visible=false
