2d9ce7a463862d6d1b9c8c6254b860ba612be1ef
Avishi Sharma <robloxpusheen8@gmail.com>
2026-06-26 10:20:06 -0400

    import settings

--- five-nights-at-ms-fishers/game_manager.gd
+++ five-nights-at-ms-fishers/game_manager.gd
@@ -45,7 +45,8 @@ func _next_hour() -> void:
 func _timeout_counter() -> void:
 	current_power-=$"../gui/usage_bar_container".power_sources*2.3
 	$"../gui/power_left".text="Power left: " + str(int(current_power/START_POWER*100)) + " %"
-	if current_power<0:
+	if current_power<0 and !game_over:
+		game_over=true
 		$power.stop()
 		$"../foxy_jumpscare".visible=true
 		$"../foxy_jumpscare/VideoStreamPlayer".play()
